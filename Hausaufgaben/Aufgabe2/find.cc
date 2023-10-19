a6cc73e4896a4dcf4c5db52a8f3b6732cf42ce29
schoellr <92320532+schoellr@users.noreply.github.com>
2023-10-19 17:40:05 +0200

    Type Parameter eingefügt in find

--- Hausaufgaben/Aufgabe2/find.cc
+++ Hausaufgaben/Aufgabe2/find.cc
@@ -2,9 +2,43 @@
 #include <sstream>
 #include <dirent.h>
 
+//Funktion Ausgabe find
+void print_files(std::string dirname, std::string pattern, std::string type, bool follow, bool xdev){
+    DIR *d; // DAteistruktur
+    struct dirent *dir; //dirent ist eintrag in Directory, sagt mir was es ist (Ordner, Datei)
+    d = opendir(dirname.c_str());
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if(std::string(dir->d_name) == "." || std::string(dir->d_name) == ".."){
+                continue;
+            }
+            else{
+                std::string temp;
+                temp += dirname + "/";
+                temp += std::string(dir->d_name);
+                if(type == "none"){
+                    printf("%s\n", temp.c_str());
+                }
+                else if (type == "f" && dir->d_type == DT_REG){
+                    printf("%s\n", temp.c_str());
+                }
+                else if (type == "d" && dir->d_type == DT_DIR){
+                    printf("%s\n", temp.c_str());
+                }
+                if (dir->d_type == DT_DIR && std::string(dir->d_name) != "." && std::string(dir->d_name) != ".."){
+                    print_files( dirname + "/" + std::string(dir->d_name), pattern, type, follow, xdev);
+                }
+            }
+
+        }
+        closedir(d);
+    }
+}
+
 // Testeingabe: find Testordner -name pattern -type f -follow -xdev
 int main(int argc, char *argv[]){
-    std::string pattern, type;
+    std::string pattern = "none";
+    std::string type = "none";
     std::string dirname = argv[1];
     //std::string str;
     bool follow = false;
@@ -35,15 +69,7 @@ int main(int argc, char *argv[]){
         std::cout << argv[i] << std::endl;
     }
 
-    DIR *d;
-    struct dirent *dir;
-    d = opendir(".");
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            printf("%s\n", dir->d_name);
-        }
-        closedir(d);
-    }
+    print_files(dirname, pattern, type, follow, xdev);
 
 
     return 0;
